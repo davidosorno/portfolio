d69fc67d00ea2564a0c6c5cdff154e715513bcff
daosgu <daosgu27@gmail.com>
2020-05-20 10:39:49 -0700

    Adding new projects.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,9 +6,9 @@ import { ContactmeComponent } from './contactme/contactme.component';
 
 
 const routes: Routes = [
-  { path: '', component: AboutmeComponent },
-  { path: 'projects', component: PortfolioComponent },
-  { path: 'contactme', component: ContactmeComponent },
+  { path: '', component: PortfolioComponent },
+  { path: 'about', component: AboutmeComponent },
+  { path: 'contact', component: ContactmeComponent },
   // redirect to /alpha if there is nothing in the url
   { path: '', pathMatch: 'full', redirectTo: ''},
   // the ** will catch anything that did not match any of the above routes
