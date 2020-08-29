b4b0a713668a5c45871423e43a987e21eb50bf4d
David <daosgu27@gmail.com>
2020-08-29 13:34:21 -0700

    Creating portfolio website

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutmeComponent } from './aboutme/aboutme.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactmeComponent } from './contactme/contactme.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AboutmeComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'contactme', component: ContactmeComponent },
+
+  // redirect to / if there is nothing in the url
+  { path: '', pathMatch: 'full', redirectTo: ''},
+  // the ** will catch anything that did not match any of the above routes
+  { path: '**', pathMatch: 'full', redirectTo: ''},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutmeComponent } from './aboutme/aboutme.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactmeComponent } from './contactme/contactme.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutmeComponent,
+    ProjectsComponent,
+    ContactmeComponent
   ],
   imports: [
     BrowserModule,
