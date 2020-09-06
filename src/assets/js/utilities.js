2a3fbb4d9f170352492bddaa3108549f7e925e8c
David <daosgu27@gmail.com>
2020-09-06 16:09:05 -0700

    Add images to aboutme and project. Enhance responsive.

--- src/assets/js/utilities.js
+++ src/assets/js/utilities.js
@@ -20,9 +20,9 @@ function initSwiper() {
         //     delay: 1500,
         //     disableOnInteraction: false,
         //   },
-        //   direction: 'vertical',
+        // direction: 'vertical',
         coverflowEffect: {
-            rotate: 70,
+            rotate: 40,
             stretch: 0,
             depth: 100,
             modifier: 1,
@@ -36,25 +36,13 @@ function initSwiper() {
         var img = element.wrapperEl.children[element.activeIndex];
         detailsProject(img.id);
     });
+    detailsProject('arvd');
 }
 
 function detailsProject(nameProject) {
-    // img-project
-    // logo-tech
     const imgProject = document.getElementsByClassName('img-project');
     for (let i = 0; i < 4; i++) {
-        var imgUrl = `url('../../assets/img/projects/${nameProject}${i + 1}`;
-        var tester = new Image();
-        try {
-            tester.onload = function () { // when .png ok
-                imgProject[i].style.backgroundImage = imgUrl + ".png')";
-            };
-            tester.onerror = function () { // when .png failed
-                imgProject[i].style.backgroundImage = imgUrl + ".jpg')";
-            };
-        } catch (err) {
-            continue;
-        }
-        tester.src = imgUrl + '.png'; // execute the test
+        var imgUrl = `url('../../assets/img/projects/${nameProject}${i + 1}.png')`;
+        imgProject[i].style.backgroundImage = imgUrl;
     }
 }
