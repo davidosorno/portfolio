179a2a10c9dbef8002c21585b648e005928cfb06
David <daosgu27@gmail.com>
2020-09-06 17:34:56 -0700

    Auto-generated commit

---
+++ assets/js/utilities.js
@@ -0,0 +1,48 @@
+const projects = {
+    0: "qyq",
+    1: "",
+    2: "",
+    3: "",
+    4: "",
+    5: "",
+    6: ""
+}
+
+function initSwiper() {
+    // It allows swiper to start working on the slides.
+    // https://github.com/nolimits4web/swiper
+    var swiper = new Swiper('.swiper-container', {
+        effect: 'coverflow',
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: 'auto',
+        // autoplay: {
+        //     delay: 1500,
+        //     disableOnInteraction: false,
+        //   },
+        // direction: 'vertical',
+        coverflowEffect: {
+            rotate: 40,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: true,
+        },
+        pagination: {
+            el: '.swiper-pagination',
+        }
+    });
+    swiper.on('transitionEnd', function (element) {
+        var img = element.wrapperEl.children[element.activeIndex];
+        detailsProject(img.id);
+    });
+    detailsProject('arvd');
+}
+
+function detailsProject(nameProject) {
+    const imgProject = document.getElementsByClassName('img-project');
+    for (let i = 0; i < 4; i++) {
+        var imgUrl = `url('../../assets/img/projects/${nameProject}${i + 1}.png')`;
+        imgProject[i].style.backgroundImage = imgUrl;
+    }
+}
